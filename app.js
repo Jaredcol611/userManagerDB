497046e9caae02e319f6865df1273b1983466eed
Jared <jaredcol13@gmail.com>
2018-05-14 23:32:27 -0600

    Dropped mongoclient for mongoose. Going well so far

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'testProject';
 const app = express();
-
-let user;
 let userArr = [];
-let userCount = 0;
 
 // use and set code
 
@@ -18,6 +15,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views',  path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+//Schema
+const mySchema = mongoose.Schema({
+    userId: String,
+    name: String,
+    email: String,
+    age: Number
+});
+
+//Mongoose beginning
+let User = mongoose.model('User', mySchema);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
+
 
 
 // express code ----------------------------------------------------------------------------
@@ -27,48 +42,24 @@ app.set('view engine', 'pug');
 
 // get root page, render index which contains form
 app.get('/', (req,res) => {
-    res.render('index', {id: userCount});
+    res.render('index');
     //check users add user button. need to reconnect to client here on button press
 });
 
 //post request pushes form data to array
 app.post('/users', (req, res) => {
-    user = {
+    let user = new User({
         userId: req.body.userId,
         name: req.body.name,
         email: req.body.email,
         age: req.body.age
-    };
-    MongoClient.connect(url, (err, client) => {
-        assert.equal(null, err);
-        console.log("Connected successfully to server");
-
-        const db = client.db(dbName);
-
-        insertDocuments(db, user, () => {
-                client.close();
-        });
     });
-
-    MongoClient.connect(url, (err, client) => {
-
-        const db = client.db(dbName);
-        const collection = db.collection('users');
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
-            console.log("Found the following records");
-            console.log(docs);
-            res.render('./users', {users:docs.map(user =>
-                    ({
-                          userId: user.data.userId,
-                          name: user.data.name,
-                          email: user.data.email,
-                          age: user.data.age
-                    }))
-                })
-          });
-            client.close();
-        });
+    user.save(function (err, user) {
+        if (err) return console.error(err);
+        console.log(user);
+    });
+    // Not using userArr anymore, will need to render the document. ??
+    res.render('./users', {users:userArr});
 });
 
 //edit page grabs id from user to render on a new form
@@ -77,7 +68,7 @@ app.get('/edit/:id', (req, res) => {
     for(let i = 0; i < userArr.length; i++){
         if(+req.params.id === userArr[i].id){
             userInfo = userArr[i];
-            res.render('./edit', {user: userInfo});
+            res.render('edit', {user: userInfo});
         }
     }
 });
@@ -88,18 +79,19 @@ app.post('/edit', (req, res) => {
         name: req.body.name,
         email: req.body.email,
         age: req.body.age,
-        //id: req.body.id
+        id: req.body.id
     };
     for(let i = 0; i < userArr.length; i++){
         if(+req.body.id === userArr[i].id){
             userArr[i] = userEdit;
         }
     }
-    res.render('users', {users:userArr});
+    res.render('users', {user:userArr});
 });
 
 // deletes specific user from array and removes them from table
 app.get('/delete/:id', (req, res) => {
+
     for(let i = 0; i < userArr.length; i++){
         if(+req.params.id === userArr[i].id){
             userArr.splice(i, 1);
@@ -116,16 +108,3 @@ app.listen(3000, () => {
 
 
 
-// All mongodb functions below -----------------------------------------------------------
-
-const insertDocuments = (db, data, callback) => {
-    // Get the documents collection
-    const collection = db.collection('users');
-    // Insert some documents
-    collection.insertOne(
-        {data}, (err, result) => {
-        assert.equal(err, null);
-        console.log("Inserted document into the collection");
-        callback(result);
-    });
-};
